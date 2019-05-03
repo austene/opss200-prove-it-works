2c0a086e0c33557033ddea3613103891dcc5c424
AE <austence@sbcglobal.net>
2019-05-03 11:01:44 -0700

    adds tests for calculator, adds functions for calculator.  Passes tests and refactors code.

--- test/unit/calculator.spec.js
+++ test/unit/calculator.spec.js
@@ -7,12 +7,39 @@ describe('Calculator', () => {
   beforeEach(() => {
     calculator = new Calculator();
   });
-
+  // Add function
   it('should have an add function', () => {
     expect(calculator.add).to.exist;
   });
 
   it('should add 2 + 2 together correctly', () => {
     expect(calculator.add(2, 2)).to.equal(4);
+  });
+
+  // Subtract function
+  it('should have a subtract function', () => {
+    expect(calculator.subtract).to.exist;
+  });
+
+  it('should subtract 4 - 2 together correctly', () => {
+    expect(calculator.subtract(4, 2)).to.equal(2);
+  });
+
+  // Multiply function
+  it('should have a multiply function', () => {
+    expect(calculator.multiply).to.exist;
+  });
+
+  it ('should multiply 2 * 2 together correctly', () => {
+    expect(calculator.multiply(2, 2)).to.equal(4);
+  });
+
+  // Divide function
+  it('should have a divide function', () => {
+    expect(calculator.divide).to.exist;
+  });
+
+  it('should divide 4 / 2 together correctly', () => {
+    expect(calculator.divide(4, 2)).to.equal(2);
   });
 });
